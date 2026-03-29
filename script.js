46b693eb9786364ce79fe7d575098c420baf8b12
Vishal Yadav <ydvishal89@gmail.com>
2026-03-29 09:41:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function volume_sphere() {
     //Write your code here
+	let num1 = document.getElementById("radius").val;
+	let vol = (4/3)3.14*num1*num1*num1;
+	document.getElementById("volume").textContent = vol;
+	
+	
   
 } 
 
